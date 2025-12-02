a07efed9a60634d901cbdd0dd53b8b9b1db237c5
Shreyas Rajaram Bhavar <shreyasbhavar0@gmail.com>
2025-12-02 22:46:18 +0530

    right angled triangle question

---
+++ 02_PRACTICE_QUESTION/04_pattern.py
@@ -0,0 +1,11 @@
+#right angled triangle
+def right_angled_triangle(n):
+    for i in range(1, n + 1):        # rows
+        for j in range(i):           # columns
+            print("*", end=" ")
+        print()                      # new line after each row
+
+
+# Taking input from user
+n = int(input("Enter the number of rows: "))
+right_angled_triangle(n)
