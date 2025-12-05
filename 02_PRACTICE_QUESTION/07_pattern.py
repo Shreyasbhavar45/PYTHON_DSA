e6ffe3903cfc8ddba4075981467ccc7e5c429d55
Shreyas Rajaram Bhavar <shreyasbhavar0@gmail.com>
2025-12-05 23:11:32 +0530

    changes

---
+++ 02_PRACTICE_QUESTION/07_pattern.py
@@ -0,0 +1,10 @@
+def inverted_pyramid(n):
+    for i in range(n):
+        # leading spaces
+        print(" " * i, end="")
+        # stars
+        print("* " * (n - i))
+
+# Example
+n = int(input("Enter n: "))
+inverted_pyramid(n)
