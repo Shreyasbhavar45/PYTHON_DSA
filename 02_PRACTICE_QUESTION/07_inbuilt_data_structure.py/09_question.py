8d7199136a379bea2bd73db52320ab721af77272
Shreyas Rajaram Bhavar <shreyasbhavar0@gmail.com>
2025-12-19 23:47:01 +0530

    changes

---
+++ 02_PRACTICE_QUESTION/07_inbuilt_data_structure.py/09_question.py
@@ -0,0 +1,11 @@
+def consecutive_max_numbers(arr):
+   max_count = 0
+   for i in range(0,lst):
+      if i in arr(lst>0):
+        max_count+=1
+      else:
+        return max_count
+        
+   
+lst = [1, 2, 3, 4, 5]
+consecutive_max_numbers(lst)
