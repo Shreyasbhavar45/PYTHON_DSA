59230561e6b3d22bf2ee70fe6ad73e2cfa3a03ec
Shreyasbhavar45 <shreyasbhavar0@gmail.com>
2026-02-23 18:10:36 +0530

    changes

--- 05_Searching_Sorting_ALgorithm/02_linear_search/02_linear_search.py
+++ 05_Searching_Sorting_ALgorithm/02_linear_search/02_linear_search.py
@@ -7,7 +7,7 @@ def linear_search(arr,target):
             return index
     
     return -1
-my_list =[10,23,45,70,11]
+my_list =[10,23,45,70,11,18]
 target = int(input("enter The number:-"))
 result =linear_search(my_list,target)
 print(result)
